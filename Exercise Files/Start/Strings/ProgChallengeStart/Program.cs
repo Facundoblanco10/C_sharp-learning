030bf9f957b1f70a3048bfaf8e89f995e419ef9c
Facu Windows <facub1011@gmail.com>
2023-03-27 16:37:39 -0300

    C# strings

--- Exercise Files/Start/Strings/ProgChallengeStart/Program.cs
+++ Exercise Files/Start/Strings/ProgChallengeStart/Program.cs
@@ -15,10 +15,37 @@ namespace ProgChallengeStart
             Console.WriteLine("Enter your guess, or -1 to give up.");
 
             // Keep track of the number of guesses and the current user guess
+            int guesses = 0;
+            int guess = -2;
 
             // Start the game and run until user quits or guesses correctly
             // HINT: You'll need a way to convert the user's input to an integer
-            
+            while (theNumber != guess && guess != -1)
+            {
+                Console.WriteLine("What's yout guess?");
+                string str = Console.ReadLine();
+                try { 
+                    guess = int.Parse(str);
+                    if (guess > theNumber) {
+                        Console.WriteLine("Is lower");
+                    }
+                    else if (guess < theNumber && guess != -1) {
+                        Console.WriteLine("Is higher");
+                    }
+                    else if (guess == theNumber) {
+                        Console.WriteLine("You win!");
+                    }
+                    else {
+                        Console.WriteLine("Coward");
+                        return;
+                    }
+                }
+                catch {
+                    Console.WriteLine("That's not a number.");
+                }
+                guesses++;
+            }
+            Console.WriteLine("Guesses so far: " + guesses);
         }
     }
 }
