2411fa995ae0685d0979af912738e2055c81bce2
Facu Windows <facub1011@gmail.com>
2023-03-27 01:17:57 -0300

    Comments

--- Exercise Files/Start/Overview/Comments/Program.cs
+++ Exercise Files/Start/Overview/Comments/Program.cs
@@ -6,11 +6,18 @@ namespace Comments
     {
         /// XML Comments are used to help provide documentation
         /// They start with triple-slashes and have a special syntax
+        /// <sumary>
+        /// This is the main sample comment
+        /// </sumary>
+        /// <param name="args">The arguments</param>
+        /// <returns>No Return Value</returns>
         static void Main(string[] args)
         {
-
+            // Single line comment
             Console.WriteLine("Hello World!");
-
+            /* Multiple line comments
+            can be used to provide documentation
+            in multi-line comments */
         }
     }
 }
